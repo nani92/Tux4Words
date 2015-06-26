bad53db308ea72ed393bc0e3f87436bd1b2aae34
Natalia Jastrzębska <nataliak@10g.pl>
2015-06-26 22:49:38 +0200

    A bit refactored root-ctrl

--- app/main/controllers/root-ctrl.js
+++ app/main/controllers/root-ctrl.js
@@ -1,97 +1,32 @@
-var rootPath = 'main/assets/json/categories/';
-
-function ReadJSONPaths(categories) {
-  var jsonURL =  rootPath + 'files.json';
-  $.getJSON(jsonURL, function (json) {
-    $.each(json.paths, function (i, path) {
-      ReadWords(path, categories);
-    });
-  });
-}
-
-function ReadWords(path, categories) {
-  var jsonURL = rootPath + path;
-  $.getJSON(jsonURL, function (json) {
-    var id = categories.getCategoryId(json.category);
-    if (id == -1) {
-      id = categories.addCategory(json.category);
-    }
-    categories.addWordsToCategoryById(id, json.words);
-  });
-}
-
-function RandomWords(words, number) {
-  images = [];
-  tmpWords = [];
-  var n = number;
-  while (n > 0) {
-    var i = Math.floor(Math.random() * words.length);
-    while (DoesLabelExists(words[i].word, tmpWords )) {
-      i = Math.floor(Math.random() * words.length);
-    }
-    tmpWords.push(words[i].word);
-    $.preloadImage(words[i].imgPath, words[i].word);
-    n--;
-  }
-}
-var images = [];
-$.preloadImage = function () {
-  images.push( $("<img />").attr("src", arguments[0]).attr("id", arguments[1]));
-}
-
-function GetLabels (inLabels, words, number) {
-  var labels = inLabels;
-  while (number > 0) {
-    number--;
-    var i = Math.floor(Math.random() * words.length);
-    while (DoesLabelExists(words[i].word, labels)) {
-      i = Math.floor(Math.random() * words.length);
-    }
-    labels.push(words[i].word);
-  }
-  return labels;
-}
-
-function DoesLabelExists(word, labels) {
-  var exist = false;
-  $.each(labels, function (index, value) {
-    if (value === word) {
-      exist = true;
-    }
-  });
-  return exist;
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-function RandomLetter (letters) {
-  var letter_set = "qwertyuiopasdfghjklzxcvbnm";
-  var i = Math.floor(Math.random() * letter_set.length);
-  while (DoesLabelExists(letter_set.substring(i, i + 1), letters)) {
-    i = Math.floor(Math.random() * letter_set.length);
-  }
-  letters.push(letter_set.substring(i, i + 1));
-  return letters;
-}
-
-var wordsPerSession = 3;
-var wordIndex;
-var isSessionStarted = false;
 angular.module('main')
 .controller('RootCtrl', function ($scope, categories, $state) {
-  console.log("RootCtrl");
+  var rootPath = 'main/assets/json/categories/';
   ReadJSONPaths(categories);
   $scope.RandomWords = RandomWords;
+  /*************************************************************/
+  /*             Reading in App Functions                      */
+  /*************************************************************/
+  function ReadJSONPaths() {
+    var jsonURL =  rootPath + 'files.json';
+    $.getJSON(jsonURL, function (json) {
+      $.each(json.paths, function (i, path) {
+        ReadWords(path, categories);
+      });
+    });
+  }
+  function ReadWords(path) {
+    var jsonURL = rootPath + path;
+    $.getJSON(jsonURL, function (json) {
+      var id = categories.getCategoryId(json.category);
+      if (id == -1) {
+        id = categories.addCategory(json.category);
+      }
+      categories.addWordsToCategoryById(id, json.words);
+    });
+  }
+  /*************************************************************/
+  /*                   Playing                                 */
+  /*************************************************************/
   $scope.StartPlay = function () {
     $scope.isLastWord = false;
     $scope.isSessionStarted = true;
@@ -111,29 +46,23 @@ angular.module('main')
   $scope.EndSession = function () {
     $scope.isSessionStarted = false;
   }
+  /*************************************************************/
+  /*                   Exercises                               */
+  /*************************************************************/
   $scope.StartExercise = function (exercise, titleId) {
     $scope.exerciseTitleId = titleId;
     if (exercise === "What is it?") {
-      WhatIsIt_Start($scope, $state, categories);
+      WhatIsIt_Start();
     }
     if (exercise === "Connect") {
       $scope.connectImages = [];
-      Connect_Start($scope, $state, categories);
+      Connect_Start();
     }
     if (exercise === "Order the letters") {
-      OrderTheLetters_Start($scope, $state, categories);
+      OrderTheLetters_Start();
     }
     if (exercise === "Type in") {
-      TypeIn_Start($scope, $state, categories);
-    }
-  }
-  $scope.WhatIsIt_Next = function () {
-    if ($scope.lifes >= 0) {
-      wordIndex++;
-      WhatIsIt($scope, $state, categories);
-    }
-    else {
-      $state.go('root.What is it?');
+      TypeIn_Start();
     }
   }
   var exerciseState;
@@ -151,87 +80,178 @@ angular.module('main')
   $scope.SetLifes = function (inLifes) {
     $scope.lifes = inLifes;
   }
+  /*************************************************************/
+  /*                   What is it?                             */
+  /*************************************************************/
+  $scope.WhatIsIt_Next = function () {
+    if ($scope.lifes >= 0) {
+      wordIndex++;
+      WhatIsIt($scope, $state, categories);
+    }
+    else {
+      $state.go('root.What is it?');
+    }
+  }
+  function WhatIsIt_Start () {
+    $scope.lifes = 3;
+    wordIndex = 0;
+    $scope.points = 0;
+    WhatIsIt();
+  }
+  function WhatIsIt () {
+    RandomWords(categories.getAllWords(), 1);
+    $scope.currentImage = images[0];
+    $scope.labels = Shuffle(GetLabels([images[0].attr('id')], categories.getAllWords(), 2));
+    $state.go('root.What is it?-Play:num', {num: wordIndex});
+  }
+  /*************************************************************/
+  /*                   Connect                                 */
+  /*************************************************************/
   $scope.Connect_Next = function () {
     wordIndex++;
     Connect($scope, $state, categories);
   }
+  function Connect_Start () {
+    $scope.lifes = 3;
+    wordIndex = 0;
+    $scope.points = 0;
+    Connect();
+  }
+  function Connect () {
+    RandomWords(categories.getAllWords(), 3);
+    $scope.connectImages = images;
+    $scope.labels = Shuffle(GetLabels(
+       [images[0].attr('id'), images[1].attr('id'), images[2].attr('id')],
+       categories.getAllWords(), 2));
+    $state.go('root.Connect-Play:num', {num: wordIndex});
+  }
+  /*************************************************************/
+  /*                   Order the letters                       */
+  /*************************************************************/
   $scope.Order_Next = function () {
     if ( $scope.lifes >= 0 ) {
       wordIndex++;
-      OrderTheLetters($scope, $state, categories);
+      OrderTheLetters();
     }
     else {
       $state.go('root.Order the letters');
     }
   }
+  function OrderTheLetters_Start () {
+    $scope.lifes = 3;
+    wordIndex = 0;
+    $scope.points = 0;
+    OrderTheLetters();
+  }
+  function OrderTheLetters () {
+    RandomWords(categories.getAllWords(), 1);
+    $scope.currentImage = images[0];
+    $scope.letters = Shuffle(images[0].attr('id').split(''));
+    $scope.frames = [];
+    $.each($scope.letters, function (key, value) {
+      $scope.frames.push(key);
+    });
+    $state.go('root.Order the letters-Play:num', {num: wordIndex});
+  }
+  /*************************************************************/
+  /*                     Type in                               */
+  /*************************************************************/
   $scope.TypeIn_Next = function () {
     if ( $scope.lifes >= 0 ) {
       wordIndex++;
-      TypeIn($scope, $state, categories);
+      TypeIn();
     }
     else {
       $state.go('root.Type in');
     }
   }
-});
-function WhatIsIt_Start ($scope, $state, categories) {
-  $scope.lifes = 3;
-  wordIndex = 0;
-  $scope.points = 0;
-  WhatIsIt($scope, $state, categories);
-}
-function WhatIsIt ($scope, $state, categories) {
-  RandomWords(categories.getAllWords(), 1);
-  $scope.currentImage = images[0];
-  $scope.labels = shuffle(GetLabels([images[0].attr('id')], categories.getAllWords(), 2));
-  $state.go('root.What is it?-Play:num', {num: wordIndex});
-}
-function Connect_Start ($scope, $state, categories) {
-  $scope.lifes = 3;
-  wordIndex = 0;
-  $scope.points = 0;
-  Connect($scope, $state, categories);
-}
-function Connect ($scope, $state, categories) {
-  RandomWords(categories.getAllWords(), 3);
-  $scope.connectImages = images;
-  $scope.labels = shuffle(GetLabels(
-     [images[0].attr('id'), images[1].attr('id'), images[2].attr('id')],
-     categories.getAllWords(), 2));
-  $state.go('root.Connect-Play:num', {num: wordIndex});
-}
-function OrderTheLetters_Start ($scope, $state, categories) {
-  $scope.lifes = 3;
-  wordIndex = 0;
-  $scope.points = 0;
-  OrderTheLetters($scope, $state, categories);
-}
-function OrderTheLetters ($scope, $state, categories) {
-  RandomWords(categories.getAllWords(), 1);
-  $scope.currentImage = images[0];
-  $scope.letters = shuffle(images[0].attr('id').split(''));
-  $scope.frames = [];
-  $.each($scope.letters, function (key, value) {
-    $scope.frames.push(key);
-  });
-  $state.go('root.Order the letters-Play:num', {num: wordIndex});
-}
-function TypeIn_Start ($scope, $state, categories) {
-  $scope.lifes = 3;
-  wordIndex = 0;
-  $scope.points = 0;
-  TypeIn($scope, $state, categories);
-}
-function TypeIn ($scope, $state, categories) {
-  RandomWords(categories.getAllWords(), 1);
-  $scope.currentImage = images[0];
-  keys = images[0].attr('id').split('');
-  while (keys.length < 7) {
-    keys = RandomLetter(keys);
+  function TypeIn_Start () {
+    $scope.lifes = 3;
+    wordIndex = 0;
+    $scope.points = 0;
+    TypeIn($scope, $state, categories);
   }
-  $scope.keys = shuffle(keys);
-  $state.go('root.Type in-Play:num', {num: wordIndex});
-}
+  function TypeIn () {
+    RandomWords(categories.getAllWords(), 1);
+    $scope.currentImage = images[0];
+    keys = images[0].attr('id').split('');
+    while (keys.length < 7) {
+      keys = RandomLetter(keys);
+    }
+    $scope.keys = Shuffle(keys);
+    $state.go('root.Type in-Play:num', {num: wordIndex});
+  }
+  /*************************************************************/
+  /*                     Helpingers                            */
+  /*************************************************************/
+  var wordsPerSession = 3;
+  var wordIndex;
+  var isSessionStarted = false;
+  function RandomWords(words, number) {
+    images = [];
+    tmpWords = [];
+    var n = number;
+    while (n > 0) {
+      var i = Math.floor(Math.random() * words.length);
+      while (DoesLabelExists(words[i].word, tmpWords )) {
+        i = Math.floor(Math.random() * words.length);
+      }
+      tmpWords.push(words[i].word);
+      $.preloadImage(words[i].imgPath, words[i].word);
+      n--;
+    }
+  }
+  var images = [];
+  $.preloadImage = function () {
+    images.push( $("<img />").attr("src", arguments[0]).attr("id", arguments[1]));
+  }
+
+  function GetLabels (inLabels, words, number) {
+    var labels = inLabels;
+    while (number > 0) {
+      number--;
+      var i = Math.floor(Math.random() * words.length);
+      while (DoesLabelExists(words[i].word, labels)) {
+        i = Math.floor(Math.random() * words.length);
+      }
+      labels.push(words[i].word);
+    }
+    return labels;
+  }
+
+  function DoesLabelExists(word, labels) {
+    var exist = false;
+    $.each(labels, function (index, value) {
+      if (value === word) {
+        exist = true;
+      }
+    });
+    return exist;
+  }
+
+  function Shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  function RandomLetter (letters) {
+    var letter_set = "qwertyuiopasdfghjklzxcvbnm";
+    var i = Math.floor(Math.random() * letter_set.length);
+    while (DoesLabelExists(letter_set.substring(i, i + 1), letters)) {
+      i = Math.floor(Math.random() * letter_set.length);
+    }
+    letters.push(letter_set.substring(i, i + 1));
+    return letters;
+  }
+});
+
 function ShowLifes($scope) {
   for (i = 3; i > 0; i--) {
     if ($scope.lifes < i ) {
