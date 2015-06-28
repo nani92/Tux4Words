4d3e44c372eb021ea8741ab5daab4d4a3f172208
Natalia Jastrzębska <nataliak@10g.pl>
2015-06-28 13:51:50 +0200

    README

--- app/main/controllers/start-ctrl.js
+++ app/main/controllers/start-ctrl.js
@@ -1,14 +1,6 @@
 //'use strict';
 angular.module('main')
 .controller('StartCtrl', function (Start, Config, $scope, $state, categories, $location, $window) {
-  // bind data from service
-  this.someData = Start.someData;
-  this.ENV = Config.ENV;
-  this.BUILD = Config.BUILD;
-
-  console.log('Hello from your Controller: StartCtrl in module main:. This is your controller:', this);
-  // TODO: do your controller thing
-  //ReadJSONPaths(categories);
   $scope.basicOptions = ['Play', 'Categories'];
   $scope.exerciseOptions = ['What is it?', 'Connect', 'Order the letters', 'Type in'];
   $scope.MoveTo = function (option, name) {
