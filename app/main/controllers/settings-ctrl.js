f375f61dde751baf2d048a51c65813ee1f373900
Natalia Jastrzębska <nataliak@10g.pl>
2015-08-20 14:56:26 +0200

    Settings

--- app/main/controllers/settings-ctrl.js
+++ app/main/controllers/settings-ctrl.js
@@ -3,4 +3,7 @@
 angular.module('main')
 .controller('SettingsCtrl', function (Start, Config, $scope, $state, categories, $location, $window) {
   $scope.title = "Settings";
+  $scope.ChangedNumberOfWordsPerSession = function () {
+    $scope.SetWordsPerSession(document.getElementById("wordsPerSession_Input").value);
+  };
 });
